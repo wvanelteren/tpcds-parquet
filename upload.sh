ed0b19448eceb14ecd468cd8204132d30c6ec58d
Vladimir Ozerov <ppozerov@gmail.com>
2022-12-26 11:03:23 +0300

    Remove upload script.

--- upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws --profile=storage-upload
-  s3 --endpoint-url=https://storage.yandexcloud.net \
-  cp --recursive \
-  $1/ s3://querifylabs.private/datasets/tpcds/1000g/parquet/$1
